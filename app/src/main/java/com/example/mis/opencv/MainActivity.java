4ef36ecf7841db11d8bc82738632bf2aad82360a
Francesco Andreussi <francesco.andreussi@gmail.com>
2018-05-23 19:44:18 +0200

    Working app
    - apk in main folder
    - screenshot of a (quite) good result

--- app/src/main/java/com/example/mis/opencv/MainActivity.java
+++ app/src/main/java/com/example/mis/opencv/MainActivity.java
@@ -5,6 +5,7 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
@@ -43,8 +44,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private MatOfRect            detectedFaces;
     private MatOfRect            detectedEyes;
     private Point                noseTipPosition;
-    private static Scalar blue;
     private static Scalar red;
+    private static Scalar green;
+    private static Scalar blue;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -55,8 +57,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
 
-                    blue = new Scalar(0,0,255);
-                    red = new Scalar(255, 0, 0);
+                    red   = new Scalar(255, 0, 0);
+                    green = new Scalar(0, 255, 0);
+                    blue  = new Scalar(0, 0, 255);
 
                     //Log.e("onManagerConnected", initAssetFile("haarcascade_frontalface_default.xml"));
                     faceCascade     = new CascadeClassifier(initAssetFile("haarcascade_frontalface_default.xml"));
@@ -90,7 +93,11 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
+        mOpenCvCameraView.setCameraIndex(1); // swapping to the frontal face camera
+
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        Toast.makeText(getApplicationContext(), "Use in Landscape Mode for good results!:)", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -127,55 +134,55 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-
-        //return inputFrame.rgba();
-        /*
         Mat col  = inputFrame.rgba();
-        Rect foo = new Rect(new Point(100,100), new Point(200,200));
-        Imgproc.rectangle(col, foo.tl(), foo.br(), new Scalar(0, 0, 255), 3);
-        return col;
-        */
+        Core.flip(col, col, 1); // flips the camera visualization in portrait mode
 
-        //Mat gray = inputFrame.gray();
-        Mat col  = inputFrame.rgba();
-
-        //Mat tmp = gray.clone();
-        //Mat tmp2 = col.clone();
-        //Imgproc.Canny(gray, tmp, 80, 100);
-        //Imgproc.cvtColor(tmp, col, Imgproc.COLOR_GRAY2RGBA, 4);
-
-        //Log.e("onCameraFrame", detectedFaces.toString());
         faceCascade.detectMultiScale(col, detectedFaces);
         for (Rect face: detectedFaces.toList()) {
-            //Log.e("FaceDetection", "faceWidth = " + face.width + " faceHeight = " + face.height);
-            if (face.area() > 1000) {
+            Log.e("FaceDetection", "faceWidth = " + face.width + " faceHeight = " + face.height + " faceArea = " + face.area());
+            if (face.area() > 20000) { // threshold to detect only foreground faces and delete some "noise-data"
+
+                //draw a rectangle arounf the face (for dubugging purposes)
                 Point pointStart = new Point(face.x, face.y);
                 Point pointEnd = new Point(face.x + face.width, face.y + face.height);
                 Imgproc.rectangle(col, pointStart, pointEnd, blue, 2);
-                eyeCascade.detectMultiScale(col, detectedEyes);
-                Rect[] eyes = detectedEyes.toArray();
-                if (eyes.length == 2){
-                    Log.d(TAG, "Whole frontal face detected!");
-                    // the average of the position of the left extreme of one eye and the right extreme
-                    // of the other give us a good estimation of the position of the center of the eyes,
-                    // not depending on the detecting order
-                    double xMidPoint = (eyes[0].x + (eyes[1].x + eyes[1].width) ) / 2;
-                    // average of the y position of the average y position of each eye
-                    double yMidPoint = ((eyes[0].y + eyes[0].height) / 2) + ((eyes[1].y + eyes[1].height) / 2) / 2;
-                    double[] noseTipCoords = {xMidPoint, (yMidPoint - face.y) / 2};
-                    noseTipPosition.set(noseTipCoords);
 
-                    double eyesMidPointDist = ((eyes[0].x + eyes[0].width) / 2) - ((eyes[1].x + eyes[1].width) / 2);
-                    int noseRadius = (int) eyesMidPointDist / 2;
+                Mat faceFrame = col.colRange(face.x, face.x + face.width).rowRange(face.y, face.y + face.height);
+                eyeCascade.detectMultiScale(faceFrame, detectedEyes);
+                Rect[] eyes = filter(detectedEyes.toList(), faceFrame); // filter noise and make type conversions
+                if(eyes != null) {
+                    for (Rect eye : eyes) {
+                        //draw a rectangle arounf the face (for dubugging purposes)
+                        pointStart = new Point(eye.x, eye.y);
+                        pointEnd = new Point(eye.x + eye.width, eye.y + eye.height);
+                        Imgproc.rectangle(faceFrame, pointStart, pointEnd, green, 2);
+                    }
+                    Log.e(TAG, eyes.length + " eyes detected");
+                    if (eyes.length == 2) {
+                        Log.d(TAG, "Whole frontal face detected!");
+                        // the average of the position of the left extreme of one eye and the right extreme
+                        // of the other give us a good estimation of the position of the center of the eyes,
+                        // not depending on the detecting order
+                        double xMidPoint = (eyes[0].x + (eyes[1].x + eyes[1].width)) / 2;
+                        // average of the y position of the average y position of each eye
+                        double yMidPoint = ((eyes[0].y + eyes[0].height) / 2) + ((eyes[1].y + eyes[1].height) / 2) / 2;
 
-                    Imgproc.circle(col, noseTipPosition, noseRadius, red);
+                        double[] noseTipCoords = {xMidPoint, (yMidPoint - face.y + face.width) / 2};
+                        noseTipPosition.set(noseTipCoords);
+
+                        double eyesMidPointDist = Math.abs(
+                                ((eyes[0].x + eyes[0].width) / 2) - ((eyes[1].x + eyes[1].width) / 2)
+                        );
+                        int noseRadius = (int) eyesMidPointDist / 2;
+                        //draw the red nose
+                        Imgproc.circle(faceFrame, noseTipPosition, noseRadius, red, -1);
+                    }
                 }
             }
         }
 
         return col;
     }
-
 
     public String initAssetFile(String filename)  {
         File file = new File(getFilesDir(), filename);
@@ -188,5 +195,22 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         } catch (IOException e) { e.printStackTrace(); }
         Log.e(TAG,"prepared local file: "+filename);
         return file.getAbsolutePath();
+    }
+
+    private Rect[] filter(List<Rect> eyesList, Mat face){
+        ArrayList<Rect> eyes = new ArrayList<>(eyesList); // necessary to remove while iterating on the eyesList
+        for(Rect eye : eyesList){
+            if (eye.area() < face.width() * face.height() / 25){ // the eye dimension is less then the 1/25-th of the face
+                eyes.remove(eye);
+            }
+        }
+        if (eyes.size() != 2){
+            return null;
+        }else{
+            // inspired to https://stackoverflow.com/questions/5374311/convert-arrayliststring-to-string-array
+            Rect[] actualEyes = new Rect[2];
+            actualEyes = eyes.toArray(actualEyes); // type conversion
+            return actualEyes;
+        }
     }
 }
